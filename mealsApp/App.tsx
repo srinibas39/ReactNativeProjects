6a46044b02eb28bb8685ead8af276ddb294c1dba
srinibas khuntia <srinibas.k@pillir.io>
2023-04-23 01:56:11 +0530

    added drawer navigation in meals app

--- mealsApp/App.tsx
+++ mealsApp/App.tsx
@@ -4,7 +4,7 @@
  *
  * @format
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
@@ -25,11 +25,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { MealsScreen } from './screens/MealsScreen';
 import { MealsDetailScreen } from './screens/MealsDetailScreen';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Favoritescreen } from './screens/FavoritesScreen';
+import Ionicons from 'react-native-ionicons';
+
+
 
 
 
 
 function App(): JSX.Element {
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -37,6 +43,30 @@ function App(): JSX.Element {
   };
 
   const Stack = createStackNavigator()
+  const Drawer = createDrawerNavigator()
+
+  const DrawerContainer = () => {
+    return <Drawer.Navigator screenOptions={{
+      headerStyle: { backgroundColor: "black" },
+      headerTintColor: "#fff",
+      sceneContainerStyle: { backgroundColor: "#454545" },
+      drawerStyle: { backgroundColor: "black" },
+      drawerInactiveTintColor: "#fff",
+      drawerActiveBackgroundColor: "#454545",
+      drawerActiveTintColor: "#fff"
+
+
+    }}>
+      <Drawer.Screen name={"All Categories"} component={CategoriesScreen} options={{
+        drawerIcon: ({ color, size }) => <Ionicons name="list" color={color} size={size} />
+
+      }} />
+      <Drawer.Screen name={"Favorites"} component={Favoritescreen} options={{
+        drawerIcon: ({ color, size }) => <Ionicons name="add" color={color} size={size} />
+      }} />
+
+    </Drawer.Navigator>
+  }
 
 
   return (
@@ -50,8 +80,8 @@ function App(): JSX.Element {
           headerTitleAlign: "center"
         }}>
           <Stack.Screen name={"mealsContainer"} options={{
-            title: "All Categories",
-          }} component={CategoriesScreen} />
+            headerShown: false,
+          }} component={DrawerContainer} />
           <Stack.Screen name={"mealsOverview"} component={MealsScreen} />
           <Stack.Screen name={"mealsDetail"} component={MealsDetailScreen} />
         </Stack.Navigator>
---
+++ mealsApp/screens/FavoritesScreen.tsx
@@ -0,0 +1,7 @@
+import { View , Text} from "react-native";
+
+export const Favoritescreen = () => {
+    return <View>
+        <Text>I am Favorites Screen</Text>
+    </View>
+}
