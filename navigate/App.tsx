2e96d9e716dc09e61797e8e0bfb9023262b1b038
srinibas khuntia <srinibas.k@pillir.io>
2023-04-22 15:15:21 +0530

    implemented bottom tab navigor

--- navigate/App.tsx
+++ navigate/App.tsx
@@ -8,6 +8,7 @@
 import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
 import {
   StyleSheet,
@@ -25,28 +26,41 @@ import { Welcome } from './components/Welcome';
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const Drawer = createDrawerNavigator();
+  // const Drawer = createDrawerNavigator();
+  const BOttomTabNavigator = createBottomTabNavigator();
 
 
   return (
     // <View>
     //   <Text>App</Text>
     // </View>
+    // <NavigationContainer>
+    //   <Drawer.Navigator screenOptions={{
+    //     headerStyle: { backgroundColor: "black" },
+    //     headerTintColor: "white",
+    //     headerTitleStyle: { fontWeight: "bold" },
+    //     drawerActiveBackgroundColor: "black",
+    //     drawerActiveTintColor: "white",
+    //     drawerStyle: { backgroundColor: "skyblue" },
+    //   }}>
+    //     <Drawer.Screen name={"welcome"} component={Welcome} options={{
+    //       drawerLabel: "Welcome Screen",
+    //     }} />
+    //     <Drawer.Screen name={"user"} component={User} />
+    //   </Drawer.Navigator>
+
+    // </NavigationContainer>
     <NavigationContainer>
-      <Drawer.Navigator screenOptions={{
+      <BOttomTabNavigator.Navigator screenOptions={{
         headerStyle: { backgroundColor: "black" },
         headerTintColor: "white",
-        headerTitleStyle: { fontWeight: "bold" },
-        drawerActiveBackgroundColor: "black",
-        drawerActiveTintColor: "white",
-        drawerStyle: { backgroundColor: "skyblue" },
       }}>
-        <Drawer.Screen name={"welcome"} component={Welcome} options={{
-          drawerLabel: "Welcome Screen",
-        }} />
-        <Drawer.Screen name={"user"} component={User} />
-      </Drawer.Navigator>
+        <BOttomTabNavigator.Screen name="welcome" component={Welcome} options={{
 
+        }} />
+        <BOttomTabNavigator.Screen name="user" component={User} />
+
+      </BOttomTabNavigator.Navigator>
     </NavigationContainer>
   );
 }
