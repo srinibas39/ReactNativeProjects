570bc9e75a60e5ae86ac31b9d5f0579dbf1b1d39
srinibas khuntia <srinibas.k@pillir.io>
2023-05-22 00:41:10 +0530

    loading overlay added

--- authApp/screens/LoginScreen.js
+++ authApp/screens/LoginScreen.js
@@ -1,9 +1,14 @@
 import AuthContent from '../components/Auth/AuthContent';
 import { login } from '../utils/auth';
+import LoadingOverlay from "../components/ui/LoadingOverlay"
+import { useState } from "react";
 
 function LoginScreen() {
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const onAuthenticate = async ({ email, password }) => {
+    setIsLoading(true)
     try {
       const token = await login({ email, password })
       console.log(token)
@@ -11,6 +16,11 @@ function LoginScreen() {
     catch (err) {
       console.log(err)
     }
+    setIsLoading(false)
+  }
+
+  if (isLoading) {
+    return <LoadingOverlay message={"Signing you in"} />
   }
 
   return <AuthContent isLogin onAuthenticate={onAuthenticate} />;
--- authApp/screens/SignupScreen.js
+++ authApp/screens/SignupScreen.js
@@ -1,10 +1,14 @@
 import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { signup } from '../utils/auth';
+import { useState } from "react";
 
 function SignupScreen() {
 
-  const onAuthenticate = async ({ email, password }) => {
+  const [isLoading, setIsLoading] = useState(false)
 
+  const onAuthenticate = async ({ email, password }) => {
+    setIsLoading(true)
     try {
       const token = await signup({ email, password })
       console.log(token)
@@ -12,7 +16,11 @@ function SignupScreen() {
     catch (err) {
       console.log(err)
     }
+    setIsLoading(false)
 
+  }
+  if (isLoading) {
+    return <LoadingOverlay message={"Signing you up"} />
   }
   return <AuthContent onAuthenticate={onAuthenticate} />;
 }
